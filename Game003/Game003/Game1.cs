fe71d4907c6eb35d0c70edb7e3ce5ea664f37a00
godefroy29 <godefroy.29@gmail.com>
2020-05-06 21:54:21 +0200

    First UP

---
+++ Game003/Game003/Game1.cs
@@ -0,0 +1,201 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+namespace Game003
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        private Texture2D background;
+        private Rectangle rectWindows;
+
+        private Player player;
+
+
+        public int WINDOW_WIDTH = 160;
+        public int WINDOW_HEIGHT = 224;
+
+        public const int BLOCK = 16;
+
+        public const int START_X = 64;
+        public const int START_Y = 208;
+
+        Matrix scale;
+        int sizeExtend = 2;
+
+        List<Rectangle> listBlockedBlock;
+
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = WINDOW_WIDTH * sizeExtend;
+            graphics.PreferredBackBufferHeight = WINDOW_HEIGHT * sizeExtend;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            rectWindows = new Rectangle(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+            listBlockedBlock = GetBlockedBlock();
+            player = new Player(Content.Load<Texture2D>("RedPlayer"),1,8, new Vector2(START_X, START_Y), WINDOW_HEIGHT, WINDOW_WIDTH, listBlockedBlock);
+            scale = Matrix.CreateScale(sizeExtend);
+            
+
+            base.Initialize();
+        }
+
+        private List<Rectangle> GetBlockedBlock()
+        {
+            List<Rectangle> res = new List<Rectangle>();
+            res.Add(CreateBlockedBlock(0, 0));
+            res.Add(CreateBlockedBlock(0, 1));
+            res.Add(CreateBlockedBlock(0, 2));
+            res.Add(CreateBlockedBlock(0, 3));
+            res.Add(CreateBlockedBlock(0, 4));
+            res.Add(CreateBlockedBlock(0, 5));
+            res.Add(CreateBlockedBlock(0, 6));
+            res.Add(CreateBlockedBlock(0, 7));
+            res.Add(CreateBlockedBlock(0, 8));
+            res.Add(CreateBlockedBlock(0, 9));
+
+            res.Add(CreateBlockedBlock(1, 0));
+            res.Add(CreateBlockedBlock(1, 3));
+            res.Add(CreateBlockedBlock(1, 9));
+
+            res.Add(CreateBlockedBlock(2, 0));
+            res.Add(CreateBlockedBlock(2, 3));
+            res.Add(CreateBlockedBlock(2, 5));
+            res.Add(CreateBlockedBlock(2, 7));
+            res.Add(CreateBlockedBlock(2, 9));
+
+            res.Add(CreateBlockedBlock(3, 0));
+            res.Add(CreateBlockedBlock(3, 3));
+            res.Add(CreateBlockedBlock(3, 9));
+            res.Add(CreateBlockedBlock(3, 10));
+
+            res.Add(CreateBlockedBlock(4, 0));
+
+            res.Add(CreateBlockedBlock(5, 0));
+            res.Add(CreateBlockedBlock(5, 5));
+            res.Add(CreateBlockedBlock(5, 7));
+            
+            res.Add(CreateBlockedBlock(6, 0));
+            res.Add(CreateBlockedBlock(6, 3));
+            res.Add(CreateBlockedBlock(6, 5));
+            res.Add(CreateBlockedBlock(6, 7));
+            res.Add(CreateBlockedBlock(6, 9));
+            res.Add(CreateBlockedBlock(6, 10));
+            
+            res.Add(CreateBlockedBlock(7, 0));
+            res.Add(CreateBlockedBlock(7, 3));
+            res.Add(CreateBlockedBlock(7, 5));
+            res.Add(CreateBlockedBlock(7, 7));
+            res.Add(CreateBlockedBlock(7, 9));
+
+            res.Add(CreateBlockedBlock(8, 0));
+            res.Add(CreateBlockedBlock(8, 3));
+            res.Add(CreateBlockedBlock(8, 9));
+
+            res.Add(CreateBlockedBlock(9, 0));
+            res.Add(CreateBlockedBlock(9, 1));
+            res.Add(CreateBlockedBlock(9, 2));
+            res.Add(CreateBlockedBlock(9, 3));
+            res.Add(CreateBlockedBlock(9, 4));
+            res.Add(CreateBlockedBlock(9, 5));
+            res.Add(CreateBlockedBlock(9, 6));
+            res.Add(CreateBlockedBlock(9, 7));
+            res.Add(CreateBlockedBlock(9, 8));
+            res.Add(CreateBlockedBlock(9, 9));
+
+
+
+
+            return res;
+        }
+
+        private Rectangle CreateBlockedBlock(int v1, int v2)
+        {
+            return new Rectangle(v1 * BLOCK, v2 * BLOCK, BLOCK, BLOCK);
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            background = Content.Load<Texture2D>("Arene1");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            
+           
+            player.Update(Keyboard.GetState());
+
+
+            //player.Move("Up");
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin(transformMatrix:scale);
+            spriteBatch.Draw(background, rectWindows, Color.White);
+            player.Draw(spriteBatch);
+
+
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Game003/Game003/Player.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game003
+{
+    class Player
+    {
+        public Texture2D Texture { get; set; }
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        private int currentFrame;
+        private int totalFrames;
+        //public const int player_spriteNumber = 8;
+        //public const int player_width = 24;
+        //public const int player_height = 32;
+        //public double speed;
+        //private int oldKey;
+
+        public const int DOWN = 0;
+        public const int UP = 1;
+        public const int LEFT = 2;
+        public const int RIGHT = 3;
+        
+        private int speed = 20;
+        private int tickCount = 0;
+
+        private const int BLOCK = 16;
+
+        Keys[] movingKeys;
+        Keys[] interactKeys;
+
+        bool interactionInProgress = false;
+
+        bool willMove = false;
+
+        bool flipWalk = false;
+        int flipWalkCount = 0;
+
+        int maxHeight;
+        int maxWidth;
+
+        Vector2 currentLocation;
+
+        KeyboardState oldState;
+        KeyboardState newState;
+
+        List<Rectangle> listBlockedBlock;
+
+        public Player(Texture2D texture, int rows, int columns, Vector2 location, int maxH, int maxW, List<Rectangle> listBlock)
+        {
+            listBlockedBlock = listBlock;
+            Texture = texture;
+            Rows = rows;
+            Columns = columns;
+            currentFrame = 0;
+            totalFrames = Rows * Columns;
+            currentLocation = location;
+            maxHeight = maxH;
+            maxWidth = maxW;
+
+            movingKeys = new[] { Keys.Left, Keys.Right, Keys.Up, Keys.Down };
+            interactKeys = new[] { Keys.Space };
+        }
+
+        public void Update(KeyboardState kState)
+        {
+            newState = kState;
+            tickCount++;
+            if (tickCount >= speed)
+            {
+                Process();
+                tickCount = 0;
+            }
+        }
+
+        private void Process()
+        {
+
+            if (interactKeys.Any(x => newState.IsKeyDown(x)))
+                InteractWithObject();
+
+            if (movingKeys.Any(x => newState.IsKeyDown(x)))
+                MoveMe();
+            
+        }
+
+        private void InteractWithObject()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void MoveMe()
+        {
+            if (interactionInProgress)
+                return;
+
+            int newFrame = 0;
+
+            if (newState.IsKeyDown(Keys.Left))
+                newFrame = LEFT;
+            if (newState.IsKeyDown(Keys.Right))
+                newFrame = RIGHT;
+            if (newState.IsKeyDown(Keys.Up))
+                newFrame = UP;
+            if (newState.IsKeyDown(Keys.Down))
+                newFrame = DOWN;
+
+            if (currentFrame % 4 == newFrame)//direction précédente identique à la nouvelle direction
+            {
+                MoveLocation(newFrame);
+            }
+
+            currentFrame = newFrame + (currentFrame >= 4 ? 0 : 4);
+            
+            
+
+            //flipWalk = currentFrame >= 4 & flipWalkCount >= 3;
+            //flipWalkCount += flipWalkCount >= 3 ? -flipWalkCount : 1;
+            //flipWalkCount++;
+
+            //currentFrame = newFrame + ((currentFrame == newFrame || currentFrame == newFrame + 4) ? currentFrame / 4 : 0);
+
+        }
+
+        private void MoveLocation(int newFrame)
+        {
+            Vector2 newLocation = currentLocation;
+            bool roadIsBlocked = false;
+            switch (newFrame)
+            {
+                case 0:
+                    newLocation.Y += currentLocation.Y >= maxHeight - BLOCK ? 0 : BLOCK;
+                    break;
+                case 1:
+                    newLocation.Y -= currentLocation.Y <= 0 ? 0 : BLOCK;
+                    break;
+                case 2:
+                    newLocation.X -= currentLocation.X <= 0 ? 0 : BLOCK;
+                    break;
+                case 3:
+                    newLocation.X += currentLocation.X >= maxWidth - BLOCK ? 0 : BLOCK;
+                    break;
+                default:
+                    break;
+            }
+            foreach (Rectangle rect in listBlockedBlock)
+            {
+                if(new Rectangle((int)newLocation.X,(int)newLocation.Y, BLOCK, BLOCK).Intersects(rect))
+                    roadIsBlocked = true;
+            }
+            currentLocation = roadIsBlocked ? currentLocation : newLocation;
+
+        }
+        
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            int width = Texture.Width / Columns;
+            int height = Texture.Height / Rows;
+            int row = (int)((float)(int)currentFrame / (float)Columns);
+            int column = (int)currentFrame % Columns;
+
+            Rectangle sourceFrame = new Rectangle(width * column, height * row, width, height);
+            Rectangle destinationRectangle = new Rectangle((int)currentLocation.X, (int)currentLocation.Y, width, height);
+
+            //spriteBatch.Draw(Texture, destinationRectangle, sourceFrame, Color.White, 0, Vector2.Zero, flipWalk ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 0);
+            spriteBatch.Draw(Texture, destinationRectangle, sourceFrame, Color.White); //, 0, Vector2.Zero, flipWalk ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 0);
+        }
+        
+
+    }
+}
